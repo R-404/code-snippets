bdcaf86c382ba60a2913bb18fc5d78dd83c3a594
renjith <renjithp141997@gmail.com>
2024-01-25 19:31:30 +0530

    javaScript: var

---
+++ javaScript/variable/details.js
@@ -0,0 +1,14 @@
+const firstName = "Maxwell Wright";
+const secondName = "Raja Villarreal";
+const thirdName = "Helen Richards";
+
+const firstPhone = "(0191) 719 6495";
+const secondPhone = "0866 398 2895";
+const thirdPhone = "0800 1111"
+
+const firstEmail = "Curabitur.egestas.nunc@nonummyac.co.uk";
+const secondEmail = "posuere.vulputate@sed.com";
+const thirdEmail = "libero@convallis.edu";
+
+console.log(firstName,"/",firstPhone,"/",firstEmail);
+console.log(thirdName,"/",thirdPhone,"/",thirdEmail);
---
+++ javaScript/variable/flower.js
@@ -0,0 +1,11 @@
+const rosePrice = 8;
+let roseCount = 70;
+const lilyPrice = 10;
+let lilyCount = 50;
+const tulipPrice = 2;
+let tulipCount = 120;
+
+
+console.log(`Rose - unit price: ${rosePrice}, quantity: ${roseCount}, value: ${rosePrice*roseCount}`);
+console.log(`Lily - unit price: ${lilyPrice}, quantity: ${lilyCount}, value: ${lilyPrice*lilyCount}`);
+console.log(`Tulip - unit price: ${tulipPrice}, quantity: ${tulipCount}, value: ${tulipPrice*tulipCount}`);
